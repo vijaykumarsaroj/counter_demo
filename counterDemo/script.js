17589be57bf8a7ab57eb6d30883738a00ab98c70
vijaykumarsaroj <61312403+vijaykumarsaroj@users.noreply.github.com>
2023-03-27 20:48:09 +0530

    Add files via upload

---
+++ counterDemo/script.js
@@ -0,0 +1,11 @@
+var current=document.querySelector('.current');
+var next=document.querySelector('.next');
+function startCounter(){
+    var interval=setInterval(animate,1000);
+}
+function animate(){
+    next.classList.add('animate');
+    setTimeout(function(){
+        next.classList.remove('animate');
+    },500);
+}
